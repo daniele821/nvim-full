a717958e7fc7567e891b9828a7ceb5f50e1ad315
Daniele Muffato <danixgithub1@gmail.com>
2025-08-03 00:12:10 +0200

    use snacks notifier

---
+++ lua/plugins/base_snacks.lua
@@ -0,0 +1,13 @@
+return {
+	"folke/snacks.nvim",
+	opts = {
+		picker = {},
+        notifier = {},
+	},
+	keys = {
+		{ "<A-f>", function() Snacks.picker.files()   end },
+		{ "<A-b>", function() Snacks.picker.buffers() end },
+		{ "<A-s>", function() Snacks.picker.grep()    end },
+		{ "<A-a>", function() Snacks.picker()         end },
+	},
+}
